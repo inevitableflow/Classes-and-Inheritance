4d2fd8e7e57c0d9b3ab193a83a426e4fb328bcd4
inevitableflow <raquelcastoldi16@gmail.com>
2023-02-09 12:36:42 -0700

    Added Program.CreateAppliance method

--- OOP Group project 1/Program.cs
+++ OOP Group project 1/Program.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP_Group_project_1
+namespace Classes_and_Appliances
 {
     internal class Program
     {
         static void Main(string[] args)
         {
         }
+        static Appliance CreateAppliance(string line)
+        {
+            Appliance a = null;
+            if (line[0] == '1')
+            {
+               // a = Refrigerator.Parse(line);
+            }
+            else if (line[0] == '2')
+            {
+               // a = Vacuum.Parse(line);
+            }
+            else if (line[0] == '3')
 
-    }
+            {
+                //a = Microwave.Parse(line);
+            }
+            else if (line[0] >= '4' && line[0] <= '5')
+
+            {
+                a = Dishwasher.Parse(line);
+            }
+            return a;
+        }
 }
