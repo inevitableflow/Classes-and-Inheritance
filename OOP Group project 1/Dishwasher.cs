d3df65e594f9bd376b94547ad432847e2f5f3d73
inevitableflow <raquelcastoldi16@gmail.com>
2023-02-09 11:52:37 -0700

    Added Dishwasher class

---
+++ OOP Group project 1/Dishwasher.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Group_project_1
+{
+    public class Dishwasher 
+    {
+        private string soundRating = string.Empty;
+        public double ItemNumber { get; set; }
+        public string Brand { get; set; }   
+        public double Quantity { get; set; }    
+        public double Wattage { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+        public string SoundRating
+        {
+            get
+            {
+                return soundRating;
+            }
+            set
+            {
+                switch (value.ToLower())
+                {
+                    case "Qt":
+                        soundRating = "Quietest";
+                        break;
+                    case "Qr":
+                        soundRating = "Quieter";
+                        break;
+                    case "Qu":
+                        soundRating = "Quiet";
+                        break;
+                    case "M":
+                        soundRating = "Moderate";
+                        break;
+                    default:
+                        throw new ArgumentException(
+                            $"{value} is not a valid sound rating. " +
+                            "Choose from: Qt (Quietest), Qr (Quieter), Qu (Quiet) or M (Moderate).");
+                }
+            }
+        }
+        public string Feature { get; set; }
+
+        public static Dishwasher Parse(string s)
+        {
+            string[] args = s.Split(';');
+
+            Dishwasher dishwasher = new Dishwasher();
+            dishwasher.ItemNumber = double.Parse(args[0]);
+            dishwasher.Brand = args[1];
+            dishwasher.Quantity = double.Parse(args[2]);
+            dishwasher.Wattage = double.Parse(args[3]);
+            dishwasher.Color = args[4];
+            dishwasher.Price = double.Parse(args[5]);
+            dishwasher.SoundRating = args[6];
+            dishwasher.Feature = args[7];
+
+            return dishwasher;
+        }
+
+
+    }
+}
+
---
+++ OOP Group project 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Group_project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
