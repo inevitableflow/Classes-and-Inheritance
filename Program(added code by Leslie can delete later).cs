9f348636aad5b3d5ca358c3f9ab547ba5f81fafb
Bubbletea386 <120143310+Bubbletea386@users.noreply.github.com>
2023-02-10 02:50:13 -0700

    Add files via upload

---
+++ Program(added code by Leslie can delete later).cs
@@ -0,0 +1,58 @@
+﻿using Classes_and_Appliances;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Inheritance.Properties
+{
+    class Program
+    {
+        //Add the program when the user types in an item number
+        static void Main(string[] args)
+        {
+            List<Appliance> appliances = ReadAppliancesFromFile("appliances.txt");
+
+            Console.WriteLine("Enter the item number of an appliance: ");
+            long itemNumber = long.Parse(Console.ReadLine());
+
+            var foundAppliance = appliances.FirstOrDefault(a => a.ItemNumber == itemNumber);
+            if (foundAppliance == null)
+            {
+                Console.WriteLine("No appliance found with that item number.");
+                return;
+            }
+
+            foundAppliance.Checkout();
+            WriteAppliancesToFile("appliances.txt", appliances);
+
+
+            // Add the program when entering brand
+
+            Console.WriteLine("Enter the brand of the appliances you want to display: ");
+            string brand = Console.ReadLine();
+
+            bool found = false;
+            foreach (Appliance appliance in appliances)
+            {
+                if (appliance.Brand.ToLower() == brand.ToLower())
+                {
+                    Console.WriteLine(appliance.ToString());
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+            {
+               
+            }
+            else
+            {
+                Console.WriteLine("No appliances brand found.");
+            }
+
+
+        }
+    }
+}
